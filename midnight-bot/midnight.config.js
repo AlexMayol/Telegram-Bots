697fcfa16350fc33afc75e2d4fe4e4f2309b67f7
Alejandro Mayol <alexmayol@hotmail.com>
2018-09-20 12:22:03 +0200

    Delete midnight.config.js

--- midnight-bot/midnight.config.js
+++
@@ -1,6 +0,0 @@
-const config = {
-    botToken: '583940541:AAGpkpw2mVO9Pn1guF34Rf6qwsqun184idE',
-    mongoURI: 'mongodb+srv://alexm:nC20dGdpKL3YtCIX@midnightbot-ksyxm.mongodb.net/midnightbot?retryWrites=true',
-    subjectID: '15950318'
-};
-module.exports = config;
