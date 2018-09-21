674d039b2d325947e66c3db3ea791d8b6a386b0e
Alejandro Mayol <alexmayol@hotmail.com>
2018-09-21 15:15:27 +0200

    more puns

--- midnight-bot/services/Texts/OW.js
+++ midnight-bot/services/Texts/OW.js
@@ -7,14 +7,19 @@ OW.words = [
     "heal","orisa","rein",
     "soldier","moira","hamster",
     "reaper","lucio","ow","briggite",
-    "mercy","roadhog","ana",
+    "mercy","roadhog","ana", "overwatch", "rankeds",
     "elo","platino","144hz","plata","oro","bronce",
     "diamante",
-    "ow",
+    "ow","quicks","junkrat"
 ]
 OW.puns = [
     "¿No te das cuenta de que a nadie le importa?", "ded game", "Te ha carrileado el gallego, ¿no?",
-    "Eres muy pesao, macho", "Cuéntaselo a quien le importe", "Prefiero beber lejía que jugar OW"
+    "Eres muy pesao, macho", "Cuéntaselo a quien le importe", "Prefiero beber lejía que jugar OW",
+    "¿Otra vez hablando de ese juego de mierda?", "A ver si maduras pronto.", "Cara anchoa", "😴😴",
+    "Lloras tanto que te mueves en canoa","Te va el cerebro con 500 de ping","Ojalá pudiera importame una mierda lo que estás diciendo.",
+    "No te leen ni tus profesores."
+
+    
 ]
 
 module.exports = OW;
