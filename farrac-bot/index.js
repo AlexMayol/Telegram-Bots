95842d72af6f18b33692dc1c8e4063ae9ab0487c
Alejandro Mayol <alexmayol@hotmail.com>
2018-12-18 23:21:48 +0100

    smash it

--- farrac-bot/index.js
+++ farrac-bot/index.js
@@ -1,13 +1,21 @@
 const TelegramBot = require("node-telegram-bot-api");
 const config = require("./farrac.config");
 
-const {WelcomeMessages, CongratulationMessages} = require ("./resources");
+const {
+  WelcomeMessages,
+  CongratulationMessages
+} = require("./resources");
 
-const { FarrantController, BirthdayController } = require("./controllers");
+const {
+  FarrantController,
+  BirthdayController
+} = require("./controllers");
 
 const database = require("./database");
 
-const bot = new TelegramBot(config.botToken, { polling: true });
+const bot = new TelegramBot(config.botToken, {
+  polling: true
+});
 
 database.connect(err => {
   if (err) {
@@ -19,20 +27,25 @@ database.connect(err => {
   console.log("Database connected!");
 });
 
-// bot.on('message', (msg) => {
-//   console.log( WelcomeMessages[Math.floor(Math.random() * WelcomeMessages.length)])
-//   //bot.sendMessage(msg.chat.id, WelcomeMessages.welcomeMessages[Math.floor(Math.random() * WelcomeMessages.length)]);
+bot.on('message', (msg) => {
+  //  console.log( "aa")
+  //  bot.sendMessage(msg.chat.id, "vale");
 
-// });
+});
 
 
-bot.onText(/\/farrant@Farrac_bot/i, msg => FarrantController.farrant(bot, msg));
 
-bot.onText(/\/add_farrant@Farrac_bot/i, msg => FarrantController.addFarrant(bot, msg));
+bot.onText(/\/farrant/i, msg => FarrantController.farrant(bot, msg));
 
-bot.onText(/\/delete_farrant@Farrac_bot/i, msg => FarrantController.deleteFarrant(bot, msg));
+bot.onText(/\/add_farrant/i, msg => FarrantController.addFarrant(bot, msg));
 
-bot.onText(/\/delete_all_farrants@Farrac_bot/i, msg => FarrantController.deleteAllFarrants(bot, msg));
+bot.onText(/\/delete_farrant/i, msg => FarrantController.deleteFarrant(bot, msg));
+
+bot.onText(/\/delete_all_farrants/i, msg => FarrantController.deleteAllFarrants(bot, msg));
+
+bot.onText(/\/smash/i, msg =>
+  bot.sendMessage(msg.chat.id, "@Farrac @Kyronne @myname_is_elena @AnaSkywalker @JoseStrife @StormBlade24 LET'S SETTLE IT IN SMASH!")
+);
 
 // bot.onText(/\/birthday@Farrac_bot/i, msg => BirthdayController.birthday(bot, msg));
 // bot.onText(/\/birthday/i, msg => BirthdayController.birthday(bot, msg));
@@ -43,12 +56,12 @@ bot.on("polling_error", error => {
 });
 
 
-bot.on("pinned_message", msg => {
-  bot.sendMessage(msg.chat.id, "¿Desde cuándo fijamos mensajes aquí?");
-});
+// bot.on("pinned_message", msg => {
+//   bot.sendMessage(msg.chat.id, "¿Desde cuándo fijamos mensajes aquí?");
+// });
 
 bot.on("new_chat_members", msg => {
-  bot.sendMessage(msg.chat.id,  WelcomeMessages[Math.floor(Math.random() * WelcomeMessages.length)]);
+  bot.sendMessage(msg.chat.id, WelcomeMessages[Math.floor(Math.random() * WelcomeMessages.length)]);
 });
 
 
